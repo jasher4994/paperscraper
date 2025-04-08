c23630a5703d57964f4ccd83c8d4aeac184259d8
jamesasher@microsoft.com <jamesasher@mac.home>
2025-04-08 16:56:46 +0100

    adding in startup file

--- app.py
+++ app.py
@@ -153,4 +153,6 @@ async def get_paper(arxiv_id: str, date: Optional[str] = None):
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
